088223e9e9b6db731f4ab65788f25173f7b7ff14
sujanpoojary <sujanpoojary884@gmail.com>
2021-04-24 14:55:20 +0530

    textfield with form validation

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,32 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     ButtonGroup,
     Button,
     Typography,
     Container,
     makeStyles,
+    TextField,
 } from "@material-ui/core";
 import { Send, KeyboardArrowRight } from "@material-ui/icons";
+import { purple } from "@material-ui/core/colors";
 
-const useStyle = makeStyles({});
+const useStyle = makeStyles({
+    filed: {
+        marginTop: 20,
+        marginBottom: 20,
+        display: "block",
+    },
+});
 
 export default function Create() {
     const classes = useStyle();
+
+    const [title, setTitle] = useState("");
+    const [details, setDetails] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(e.target);
+    };
+
     return (
         <Container>
             <Typography variant="h6" color="textSecondary" align="left">
                 Create a note..
             </Typography>
-            <Button
-                className={classes.btn}
-                type="submit"
-                color="primary"
-                variant="contained"
-                endIcon={<KeyboardArrowRight />}
-            >
-                Submit
-            </Button>
-            <br />
+            <form autoComplete="off" onSubmit={handleSubmit}>
+                <TextField
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    className={classes.filed}
+                    label="Note title..."
+                    variant="outlined"
+                    color="secondary"
+                    fullWidth
+                    required
+                    error={title ? false : true}
+                />
+                <TextField
+                    value={details}
+                    onChange={(e) => setDetails(e.target.value)}
+                    className={classes.filed}
+                    label="Details.."
+                    variant="outlined"
+                    color="secondary"
+                    multiline
+                    rows={4}
+                    fullWidth
+                    required
+                    error={title ? false : true}
+                />
+                <Button
+                    className={classes.btn}
+                    type="submit"
+                    color="secondary"
+                    variant="contained"
+                    endIcon={<KeyboardArrowRight />}
+                >
+                    Submit
+                </Button>
+            </form>
         </Container>
     );
 }
