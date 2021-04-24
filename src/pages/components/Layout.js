9c1afd89644270ad24719c4bd8e449cfa402f572
sujanpoojary <sujanpoojary884@gmail.com>
2021-04-24 22:31:38 +0530

    dside drawer and manu

---
+++ src/pages/components/Layout.js
@@ -0,0 +1,89 @@
+import {
+    makeStyles,
+    Drawer,
+    Typography,
+    List,
+    ListItem,
+    ListItemText,
+    ListItemIcon,
+} from "@material-ui/core";
+import { AddOutlined, SubjectOutlined } from "@material-ui/icons";
+import React from "react";
+import { useHistory, useLocation } from "react-router";
+
+const drawerWidth = 240;
+
+const useStyle = makeStyles({
+    page: {
+        background: "#f9f9f9",
+        width: "100%",
+    },
+    drawer: {
+        width: drawerWidth,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    root: {
+        display: "flex",
+    },
+    layoutTitle: {
+        textAlign: "center",
+    },
+    activeLink: {
+        backgroundColor: "#f4f4f4",
+    },
+});
+
+const Layout = (props) => {
+    const classes = useStyle();
+    const history = useHistory();
+    const location = useLocation();
+    const menuItems = [
+        {
+            text: "My notes",
+            icon: <SubjectOutlined color="secondary" />,
+            path: "/",
+        },
+        {
+            text: "Create note",
+            icon: <AddOutlined color="secondary" />,
+            path: "/create",
+        },
+    ];
+
+    return (
+        <div className={classes.root}>
+            <Drawer
+                className={classes.drawer}
+                variant="permanent"
+                anchor="left"
+                classes={{ paper: classes.drawerPaper }}
+            >
+                <div className={classes.layoutTitle}>
+                    <Typography variant="h5">Ninjs notes..</Typography>
+                </div>
+                <List>
+                    {menuItems.map((menu) => (
+                        <ListItem
+                            button
+                            key={menu.text}
+                            onClick={() => history.push(menu.path)}
+                            className={
+                                location.pathname == menu.path
+                                    ? classes.activeLink
+                                    : null
+                            }
+                        >
+                            <ListItemIcon>{menu.icon}</ListItemIcon>
+                            <ListItemText primary={menu.text} />
+                        </ListItem>
+                    ))}
+                </List>
+            </Drawer>
+            <div className={classes.page}>{props.children}</div>
+        </div>
+    );
+};
+
+export default Layout;
